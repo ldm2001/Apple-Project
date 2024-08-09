802417bdf4ac58a9f8e53af51bc1267e52773036
이동민 (Dong Min Lee) <162676046+ldm2001@users.noreply.github.com>
2024-08-09 18:52:18 +0900

    Delete member directory

--- member/inc_footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-  <footer class="bg-dark mt-4 p-5 text-center" style="color: #FFFFFF;">
-		Copyright &copy; 2024 Lee Dong Min All Rights Reserved.
-      </div>
-    </div>
-</footer>
-</body>
-</html>
